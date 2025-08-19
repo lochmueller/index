a7bc48750694c412d3531a85c81fd7c3cbeb8d14
Tim Lochmüller <tim.lochmueller@hdnet.de>
2025-08-19 22:04:19 +0200

    chore: Change ext_emconf.php version to 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Index',
     'description' => 'Smart and flexible async indexing of pages and documents for e.g. search engines or AI provider',
-    'version' => '0.0.2',
+    'version' => '0.1.0',
     'category' => 'be',
     'constraints' => [
         'depends' => [
